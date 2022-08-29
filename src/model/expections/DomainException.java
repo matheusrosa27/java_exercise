8dccdd566b25b4c700c8ecf3e0583a16d7d0a102
Matheus Fernandes da Rosa <matheusmemphis@gmail.com>
2022-08-29 13:22:27 -0300

    creating expections and try-catch-finally

---
+++ src/model/expections/DomainException.java
@@ -0,0 +1,9 @@
+package model.expections;
+
+public class DomainException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String message) {
+        super(message);
+    }
+}
